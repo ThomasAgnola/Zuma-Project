e88fb206d416aeafb84e46b5ccb92397c0f573d7
AlexandreMouzouni <58190342+AlexandreMouzouni@users.noreply.github.com>
2021-11-25 15:56:55 +0100

    Collision
    Ajout d'une collision

--- Assets/Scripts/SplineWalker.cs
+++ Assets/Scripts/SplineWalker.cs
@@ -5,24 +5,60 @@ public class SplineWalker : MonoBehaviour
 
     public BezierSpline spline;
 
+    public bool isCollision = false;
     public float duration;
 
     private float progress;
 
     public bool lookForward;
 
+
+    void OnCollisionEnter(Collision collision)
+    {
+        /*
+        switch(collision.gameObject.tag)
+        {
+            case "cube":
+                isCollision = true;
+                Debug.Log("collsion cube avec"+ collision.gameObject.tag);
+                break;
+            case "cube1":
+                isCollision = true;
+                Debug.Log("collsion cube avec" + collision.gameObject.tag);
+                break;
+            case "cube2":
+                isCollision = true;
+                Debug.Log("collsion cube avec" + collision.gameObject.tag);
+                break;
+        }
+       */
+        if (collision.gameObject.name == "Cube")
+        {
+            isCollision = true;
+
+            Debug.Log("collsion cube avec" + collision.gameObject.tag);
+        }
+        
+    }
+
+    
     private void Update()
     {
-        progress += Time.deltaTime / duration;
-        if (progress > 1f)
+        if (isCollision == false)
         {
-            progress = 1f;
+            progress += Time.deltaTime / duration;
+                    if (progress > 1f)
+                    {
+                        progress = 1f;
+                    }
+                    Vector3 position = spline.GetPoint(progress);
+                    transform.localPosition = position;
+                    if (lookForward)
+                    {
+                        transform.LookAt(position + spline.GetDirection(progress));
+                    }
         }
-        Vector3 position = spline.GetPoint(progress);
-        transform.localPosition = position;
-        if (lookForward)
-        {
-            transform.LookAt(position + spline.GetDirection(progress));
-        }
+        isCollision = false;
+        
     }
 }
