c34e1da32827bff82f099b2831c1b840d349e09c
ThomasAgnola <thomas.agnola@edu.esiee.fr>
2021-12-05 23:29:53 +0100

    didn't saved MoveForward scipt and added a preliminary class for Ball list object (might be used / to determine )

---
+++ Assets/Scripts/Class/Balls.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Balls : IComparable<Balls>
+{
+    public string name;
+    public int index;
+
+    public Balls(string newName, int newindex)
+    {
+        name = newName;
+        index = newindex;
+    }
+
+    //This method is required by the IComparable
+    //interface. 
+    public int CompareTo(Balls other)
+    {
+        if (other == null)
+        {
+            return 1;
+        }
+
+        //Return the difference in power.
+        return index - other.index;
+    }
+}
